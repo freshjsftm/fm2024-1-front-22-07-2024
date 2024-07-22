7c8d7bab93da8e640bcb3a464a2264dc05af8b01
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-22 11:44:57 +0300

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Footer.module.sass';
 import CONSTANTS from '../../constants';
 
-class Footer extends Component {
-  topFooterItemsRender = item => (
+const Footer = () => {
+  const topFooterItemsRender = (item) => (
     <div key={item.title}>
       <h4>{item.title}</h4>
-      {item.items.map(i => (
-        <a key={i} href='https://google.com'>
+      {item.items.map((i) => (
+        <a key={i} href="https://google.com">
           {i}
         </a>
       ))}
     </div>
   );
 
-  topFooterRender () {
-    return CONSTANTS.FooterItems.map(item => this.topFooterItemsRender(item));
-  }
+  const topFooterRender = () => {
+    return CONSTANTS.FooterItems.map((item) => topFooterItemsRender(item));
+  };
 
-  render () {
-    return (
-      <div className={styles.footerContainer}>
-        <div className={styles.footerTop}>
-          <div>{this.topFooterRender()}</div>
-        </div>
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerTop}>
+        <nav>{topFooterRender()}</nav>
       </div>
-    );
-  }
-}
+    </footer>
+  );
+};
 
 export default Footer;
